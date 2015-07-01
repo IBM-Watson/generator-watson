159ef397eb98c359dd5a97cc201cfefc11b1df51
Sam Richard <snugug@gmail.com>
2015-06-30 19:16:03 -0500

    :white_check_mark: Add tests for bower.json and package.json

--- test/test-app.js
+++ test/test-app.js
@@ -1,15 +1,16 @@
 'use strict';
 
-var path = require('path');
-var assert = require('yeoman-generator').assert;
-var helpers = require('yeoman-generator').test;
-var os = require('os');
+var path = require('path'),
+    assert = require('yeoman-generator').assert,
+    helpers = require('yeoman-generator').test,
+    fs = require('fs'),
+    os = require('os');
 
 describe('Watson:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
       .withOptions({ skipInstall: true })
-      .withPrompts({ someOption: true })
+      .withPrompts({ project: 'Hello World' })
       .on('end', done);
   });
 
@@ -17,8 +18,36 @@ describe('Watson:app', function () {
     assert.file([
       'bower.json',
       'package.json',
-      '.editorconfig',
-      '.jshintrc'
+      '.editorconfig'
     ]);
+  });
+
+  it('templates bower.json', function () {
+    assert.fileContent('bower.json',
+                        '{' + '\n' +
+                        '  "name": "hello-world",' + '\n' +
+                        '  "version": "0.0.0",' + '\n' +
+                        '  "dependencies": {}' + '\n' +
+                        '}');
+  });
+
+  it('templates package.json', function () {
+    assert.fileContent('package.json',
+                        '{' + '\n' +
+                        '  "name": "hello-world",' + '\n' +
+                        '  "version": "0.0.0",' + '\n' +
+                        '  "scripts": {' + '\n' +
+                        '    "postinstall": "npm run install:bower",' + '\n' +
+                        '    "install:bower": "npm run bower install",' + '\n' +
+                        '    "bower": "./node_modules/bower/bin/bower",' + '\n' +
+                        '    "gulp": "./node_modules/gulp/bin/gulp.js"' + '\n' +
+                        '  },' + '\n' +
+                        '  "devDependencies": {' + '\n' +
+                        '    "gulp": "^3.8.11",' + '\n' +
+                        '  },' + '\n' +
+                        '  "dependencies": {' + '\n' +
+                        '    "bower": "^1.4.1"' + '\n' +
+                        '  }' + '\n' +
+                        '}');
   });
 });
